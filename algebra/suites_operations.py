8bca8891b5bbc09f197cca12cbbcfff4d309e084
Milan Assuied <milan.assuied@gmail.com>
2020-04-19 22:48:50 -0400

    Rework the function in order to use the mathematical expression

--- algebra/suites_operations.py
+++ algebra/suites_operations.py
@@ -2,11 +2,13 @@ ZERO_TO_ONE_BASE_SHIFT = 1
 
 def compute_sum_of_the_first_integers(upper_boundary):
 
-    numbers = range(upper_boundary + ZERO_TO_ONE_BASE_SHIFT)
+    if not isinstance(upper_boundary, int):
+        raise TypeError("Provided value should be an int, received an {}".format(upper_boundary.__class__.__name__))
 
-    result = 0
-    for number in numbers:
-        result = result + number
+    if upper_boundary <= 0:
+        result = 0
+    else:
+        result = int(upper_boundary*(upper_boundary+1)/2)
 
     return result
 
