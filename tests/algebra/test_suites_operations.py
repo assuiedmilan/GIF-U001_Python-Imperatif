1561594fe1a375f1450b7c0afd7524a6d0fd8e91
Milan Assuied <milan.assuied@opal-rt.com>
2020-04-19 22:53:09 -0400

    Update test

--- tests/algebra/test_suites_operations.py
+++ tests/algebra/test_suites_operations.py
@@ -6,10 +6,13 @@ from algebra.suites_operations import compute_sum_of_the_first_integers, print_s
 def test_compute_sum_of_the_first_integers(integer_to_test, expected_results):
     assert compute_sum_of_the_first_integers(integer_to_test) == expected_results
 
-@pytest.mark.parametrize("integer_to_test", [9.5, "9"])
-def test_compute_sum_of_the_first_integers_invalid_entries(integer_to_test):
-    with pytest.raises(TypeError):
-        compute_sum_of_the_first_integers(integer_to_test)
+@pytest.mark.parametrize("upper_bound, class_received", [(9.5, "float"), ("9", "str")])
+def test_compute_sum_of_the_first_integers_invalid_inputs(upper_bound, class_received):
+
+    with pytest.raises(TypeError) as execution_info:
+        compute_sum_of_the_first_integers(upper_bound)
+
+    assert "Provided value should be an int, received an {}".format(class_received) in str(execution_info.value)
 
 @pytest.mark.parametrize("integer_to_test, expected_results", [(9, "45\n")])
 def test_print_sum_of_the_first_integers(integer_to_test, expected_results, capsys):
@@ -17,7 +20,10 @@ def test_print_sum_of_the_first_integers(integer_to_test, expected_results, caps
     capture_from_stdout = capsys.readouterr()
     assert capture_from_stdout.out == expected_results
 
-@pytest.mark.parametrize("integer_to_test", [9.5, "9"])
-def test_print_sum_of_the_first_integers_invalid_entries(integer_to_test):
-    with pytest.raises(TypeError):
-        print_sum_of_the_first_integers(integer_to_test)
+@pytest.mark.parametrize("upper_bound, class_received", [(9.5, "float"), ("9", "str")])
+def test_print_sum_of_the_first_integer_invalid_inputs(upper_bound, class_received):
+
+    with pytest.raises(TypeError) as execution_info:
+        print_sum_of_the_first_integers(upper_bound)
+
+    assert "Provided value should be an int, received an {}".format(class_received) in str(execution_info.value)
