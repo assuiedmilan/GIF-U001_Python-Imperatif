da7431ae4b32a25745207ae372e8e5904686813e
Milan Assuied <milan.assuied@opal-rt.com>
2020-12-20 14:38:27 -0500

    Reorganize

--- main.py
+++
@@ -1,6 +0,0 @@
-from src.algebra_functions import operations
-
-if __name__ == "__main__":
-    operations.print_sum_of_the_first_integers(9)
-    operations.print_sum_of_the_first_integers(100)
-    operations.print_sum_of_the_first_integers(-5)
